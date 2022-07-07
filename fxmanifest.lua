bf59b983ac3691f06c9897d6feaeca2cac1e32d9
ndc2510 <ndc.25101998@gmail.com>
2022-07-07 08:09:19 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,11 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'tus.dcdev#9999'
+description 'tus.dcdev#9999 & Ice Cream#0973'
 
 version '1.0.0'
 
-data_file 'DLC_ITYP_REQUEST' 'stream/carrot_y.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/carrot_y.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'carrot_y.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'carrot_y.ytyp'
 
 lua54 'yes'
