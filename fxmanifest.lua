aa50d35520b38382afd0535c50f2a9bc2b43f0ee
ndc2510 <ndc.25101998@gmail.com>
2022-07-07 08:07:42 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'tus.dcdev#9999'
+
+version '1.0.0'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/carrot_y.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/carrot_y.ytyp'
+
+lua54 'yes'
